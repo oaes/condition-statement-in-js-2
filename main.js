8ac3215d430b20adcdacda96a6a6f888c927b159
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-06 23:09:31 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,12 @@
+
+//---------------else-if statement--------------//
+
+var x = 80
+var z = 80
+if(x>z){
+    console.log('x is greater than z')
+}else if(x<z){
+    console.log('z is greater than x')
+}else{
+    console.log('they both are same')
+}
